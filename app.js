6a4c3c8ec3b4f8637b9b7d42daeb7a9d5ffdffb8
LaurenceLama <laurencelamaFSE@gmail.com>
2023-02-05 13:35:15 +0800

    Completed Advanced E-portfolio(100% funcitonal), Small edit and other changes when times comes on including MY produced projects

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ function moveBackground(event) {
   for (let i = 0; i < shapes.length; i++) {
     const isOdd = i % 2 !== 0;
     const boolInteger = isOdd ? -1 : 1;
-    shapes[i].style.transform = `translate(${x * boolInteger}px, ${y * boolInteger}px) rotate(${x * boolInteger * 10}deg)`;
+    shapes[i].style.transform = `translate(${x * boolInteger}px, ${
+      y * boolInteger
+    }px) rotate(${x * boolInteger * 10}deg)`;
   }
 }
 
@@ -40,4 +42,36 @@ function moveBackground(event) {
  * rotate(${y * boolInteger * 10}deg)`
  */
 
-// Functional modal, it is finally working thanks to dc coder chads, Imma start later tho
+// Functional modal
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+//   when putting classes on a String, put space instead of .
+  emailjs
+    .sendForm(
+      "service_kueprne",
+      "template_gwbb11yy",
+      event.target,
+      "6bb0lY29GPPPQglEn"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    //   since success does not have a classList above(1st time putting success.classList), we also put space here instead of .
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please try again or contact me directly at lalalalaa@lala.lala"
+      );
+    });
+
+  //   loading.classList += " modal__overlay--visible";
+  //   setTimeout(() => {
+  //     loading.classList.remove("modal__overlay--visible");
+  //     success.classList += "modal__overlay--visible";
+  //     console.log("worked ma boi");
+  //   }, 1000);
+}
