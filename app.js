253e50f7b5040f6e869378b45b2e9d6ec4b7270c
LaurenceLama <laurencelamaFSE@gmail.com>
2023-02-05 12:16:36 +0800

    Initialized complete e-portfolio , except for functional modal

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// I wished to follow along the rest of the video from using emailjs.com, my newly created account does not able to be provided with a confirmation email request. I've tried everything, even made a 2nd account, still did not work, send help..
+let isModalOpen = false;
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = !isModalOpen;
+  document.body.classList += "modal--open";
+}
+
+// dark mode
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += "dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+// mouse move effect
+const scaleFactor = 1 / 20;
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInteger = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInteger}px, ${y * boolInteger}px) rotate(${x * boolInteger * 10}deg)`;
+  }
+}
+
+/**
+ * You can put this beside x rotate if ya want
+ * rotate(${y * boolInteger * 10}deg)`
+ */
+
+// Functional modal, it is finally working thanks to dc coder chads, Imma start later tho
